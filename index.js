c411d911d74d1428e7619b17670111c9937d4b65
Milena Kari <milenakari@yahoo.com>
2019-08-16 18:27:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,9 @@ let drivers = ["Milo", "Otis", "Garfield"]
 function destructivelyAppendDriver(name) {
   return drivers.push(name);
 }
+
+function destructivelyPrependDriver(name) {
+  return drivers.unshift(name);
+}
+
+function 
